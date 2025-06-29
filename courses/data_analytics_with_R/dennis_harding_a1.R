8e03c1733a6bed54acbca68c2e8c51a79d58cfb1
Dennis Harding <dennis.harding2@gmail.com>
2025-06-29 20:30:45 +0200

    Assignment 1 first draft.

--- courses/data_analytics_with_R/dennis_harding_a1.R
+++ courses/data_analytics_with_R/dennis_harding_a1.R
@@ -1,8 +1,9 @@
 library(tidyverse)
+library(ggplot2)
 data(msleep)
 ?msleep
 
-msleep
+summary(msleep)
 
 msleep_sorted <- msleep %>% arrange(sleep_total)
 msleep_sorted
@@ -20,15 +21,29 @@ shortest_sleep_mammal <-
 missing_values <- max(colSums(is.na(msleep)))
 most_missing <- names(msleep[which.max(colSums(is.na(msleep)))])
 most_missing
+
 # 4. Correlations
-correlations <- ...
+numeric <- msleep %>% select(where(is.numeric))
+correlations <- cor(numeric, use = "pairwise.complete.obs", method = "pearson")
+correlations
 
 # 5. Highest correlation
+
 correlations_copy <- correlations
-highest_corr <- ...
+correlations_copy[correlations_copy == 1] <- 0
+correlations_copy[correlations_copy == min(correlations_copy)] <- 0
+correlations_copy
+highest_corr <- max(correlations_copy)
 
 # 6. Sleep time distribution
-sleep_histogram <- ...
+sleep_histogram <- ggplot(data = msleep, aes(x = sleep_total)) +
+  geom_histogram(binwidth = 1) +
+  labs(title = "Distribution of tot. sleep",
+       x = "Total Sleep (hours per day)",
+       y = "Count") +
+  theme_minimal()
+print(sleep_histogram)
+
 
 # 7. Bar chart for food categories
 food_barchart <- ...
